f1e6a1ba74932ab3c57bd41a2a422d6febb3397a
Nate Lovell <Natelovell99@gmail.com>
2021-08-04 23:19:34 -0600

    Create Scale Clip to Sequence.jsx

---
+++ Scale Clip to Sequence.jsx
@@ -0,0 +1,14 @@
+﻿function scaleClipToSequence(clip) {
+    
+    var sequence = app.project.activeSequence;
+    var clipWidth = 1920;
+    var clipHeight = 1080;
+    var myScale = 100;
+    //clip.scaleToFrameSize = true;
+    var x = sequence.frameSizeHorizontal / clipWidth * 100;
+    var y = sequence.frameSizeVertical / clipHeight * 100;
+    
+    clip.components[1].properties[3].setValue(false, true);
+    clip.components[1].properties[1].setValue(y, true);
+    clip.components[1].properties[2].setValue(x, true);
+    }
